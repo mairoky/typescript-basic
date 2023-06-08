d8fe9f52539eaae0984db18fd09b625391412231
Aminul Islam Rocky <mairoky@outlook.com>
2023-06-09 00:29:15 +0600

    nested object & optional property

--- object/objects.ts
+++ object/objects.ts
@@ -20,3 +20,38 @@ const printName = (person: { first: string; last: string }): string => {
   return `Person Name is ${person.first} ${person.last}`;
 };
 console.log(printName({ first: "Thomas", last: "Shelby" }));
+
+// Nested Object
+
+type Song = {
+  title: string;
+  singer: string;
+  viewsCount: number;
+  credit: {
+    producer: string;
+    writer: string;
+  };
+};
+
+function calculateEarning(song: Song): number {
+  return song.viewsCount * 0.1;
+}
+
+const mySong: Song = {
+  title: "Numb",
+  singer: "Linkin Park",
+  viewsCount: 2563521,
+  credit: {
+    producer: "Mr Asdf",
+    writer: "Mr Zxc",
+  },
+};
+
+// Optional property
+type MyPoint = {
+  x: number;
+  y: number;
+  z?: number;
+};
+
+const myExPoint: MyPoint = { x: 12, y: 23, z: 33 };
